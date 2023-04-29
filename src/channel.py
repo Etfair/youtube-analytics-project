d1f30c9ee96a40bf04309457d6b535fbf4990e6d
Etfair <and90dmb@Mail.ru>
2023-04-29 21:19:25 +0300

    complete vers.2 homework-2

--- src/channel.py
+++ src/channel.py
@@ -7,9 +7,17 @@ class Channel:
     """Класс для ютуб-канала"""
     API_KEY: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=API_KEY)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
+        self.channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel_data['items'][0]['snippet']['title']
+        self.description = self.channel_data['items'][0]['snippet']['description']
+        self.url = "https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA"
+        self.subscribers = self.channel_data['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel_data['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel_data['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -29,30 +37,6 @@ class Channel:
     @classmethod
     def get_service(cls):
         return build('youtube', 'v3', developerKey=cls.API_KEY)
-
-    @property
-    def title(self):
-        return self.print_info()["items"][0]["snippet"]["title"]
-
-    @property
-    def description(self):
-        return self.print_info()["items"][0]["snippet"]["description"]
-
-    @property
-    def url(self):
-        return f'https://www.youtube.com/channel/{self.__channel_id}'
-
-    @property
-    def subs(self):
-        return self.print_info()["items"][0]["statistics"]["subscriberCount"]
-
-    @property
-    def video_count(self):
-        return self.print_info()["items"][0]["statistics"]["videoCount"]
-
-    @property
-    def viewers(self):
-        return self.print_info()["items"][0]["statistics"]["viewCount"]
 
     @property
     def channel_id(self):
