555225869e819365a00bf10ccc27adf94b6bcf60
Etfair <and90dmb@Mail.ru>
2023-05-17 20:29:26 +0300

    complete homework 6

--- src/video.py
+++ src/video.py
@@ -1,6 +1,6 @@
 import os
 from googleapiclient.discovery import build
-
+from googleapiclient.errors import HttpError
 
 class Video:
     """
@@ -11,14 +11,37 @@ class Video:
 
     def __init__(self, video_id: str):
         self.video_id = video_id
-        self.video_info = self.get_info()
-        self.video_name = self.video_info["items"][0]["snippet"]["title"]
-        self.video_link = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.video_views = self.video_info["items"][0]["statistics"]["viewCount"]
-        self.video_likes = self.video_info["items"][0]["statistics"]["likeCount"]
+
+        try:
+            self.video_link = f'https://www.youtube.com/watch?v={self.video_id}'
+
+        except HttpError:
+            self.video_info = None
+            self.title = None
+            self.video_link = None
+            self.video_views = None
+            self.like_count = None
+
+        try:
+            self.video_info = self.youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
+            self.title = self.video_info['items'][0]['snippet']['title']
+
+        except IndexError:
+            self.video_info = None
+            self.title = None
+            self.video_link = None
+            self.video_views = None
+            self.like_count = None
+
+        else:
+            self.video_info = self.youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
+            self.title = self.video_info['items'][0]['snippet']['title']
+            self.video_link = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.video_views = self.video_info["items"][0]["statistics"]["viewCount"]
+            self.like_count = self.video_info["items"][0]["statistics"]["likeCount"]
 
     def __str__(self):
-        return self.video_name
+        return self.title
 
     def get_info(self):
         """
